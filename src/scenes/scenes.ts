0bec5af4bfc2957acb8c0caafe2d49bc82c4a50a
aseruneko <aseruneko@gmail.com>
2021-09-18 16:58:32 +0900

    Revert "initial commit"
    This reverts commit efb6a7187bb3f51a982ce0f4a654f45a55a5ed0e.

--- src/scenes/scenes.ts
+++
@@ -1,6 +0,0 @@
-import Main from './main-scene';
-import Loading from './loading-scene';
-import Title from './title-scene';
-
-export default [Loading, Main, Title];
-export { Main, Loading, Title };
